6359556e1697e53787743b91b67a3e8b14d76094
jswitten <witten.jacob@gmail.com>
2020-03-01 16:00:03 -0500

    Includes comparison of embeddings vs none
    see compare_multispecies_to_single_species_training method to compare results on test set when: 1) training on all data from all species vs 2) training only on data from a specific species. Spoiler, the multi-species doesn't really make any difference!

--- src/embedding_model.py
+++ src/embedding_model.py
@@ -73,20 +73,83 @@ def make_PCA_plot(species_embeddings,species_tags,colors):
 	plt.savefig('../species_embeddings.png')
 	plt.show()
 
+def compare_multispecies_to_single_species_training(species,test_size=0.25):
+	amp_data = get_amp_data_training_set()
+	early_stopping = EarlyStopping(monitor = 'val_loss', patience = 15)
 
-def generate_pca_plot_from_data(amp_data,embed_dim=10):
-	# Generates species embeddings based on the entire dataset (rather than a training test split, as this isn't really a training/test sort of thing)
+	amp_test_data = amp_data[0:int(len(amp_data)*test_size)]
+	amp_train_data = amp_data[int(len(amp_data)*test_size):]
+	amp_train_data_species_only = amp_train_data[amp_train_data.bacterium.isin([species])]
+	amp_test_data = amp_test_data[amp_test_data.bacterium.isin([species])]
+	amp_train_data = amp_train_data[~amp_train_data.sequence.isin(amp_test_data.sequence)]
+
+	all_train_x0 = np.array([sequence_to_vector(s.upper()) for s in amp_train_data['sequence'].values])
+	species_only_train_x0 = np.array([sequence_to_vector(s.upper()) for s in amp_train_data_species_only['sequence'].values])
+	test_x0 = np.array([sequence_to_vector(s.upper()) for s in amp_test_data['sequence'].values])
+	all_train_y = amp_train_data.value.values
+	species_only_train_y = amp_train_data_species_only.value.values
+	test_y = amp_test_data.value.values
+	all_train_x2 = amp_train_data.reset_index()['bacterium_id']
+	species_only_train_x2 = amp_train_data_species_only.reset_index()['bacterium_id']
+	test_x2 = amp_test_data.reset_index()['bacterium_id']
+
+	# Train model using species only
+	species_only_embed_model = conv_model_species_embedding()
+	species_only_embed_model.fit([species_only_train_x0,species_only_train_x2],species_only_train_y,epochs=40,validation_split=0.1,callbacks=[early_stopping])
+	# Train model using embeddings & all data
+	all_embed_model = conv_model_species_embedding()
+	all_embed_model.fit([all_train_x0,all_train_x2],all_train_y,epochs=40,validation_split=0.1,callbacks=[early_stopping])
+
+	# Evaluate
+	print('Error from all species training: ' +repr(all_embed_model.evaluate([test_x0,test_x2],test_y)))
+	print('Error from training on only one species: '+repr(species_only_embed_model.evaluate([test_x0,test_x2],test_y)))
+
+	# Plot
+	plt.plot(all_embed_model.predict([test_x0,test_x2]),test_y,'.')
+	plt.savefig('All_data_for_training.png')
+	plt.title('Error: '+repr(all_embed_model.evaluate([test_x0,test_x2],test_y)))
+	plt.show()
+	plt.plot(species_only_embed_model.predict([test_x0,test_x2]),test_y,'.')
+	plt.savefig('Species_only_data_for_training.png')
+	plt.title('Error: '+repr(species_only_embed_model.evaluate([test_x0,test_x2],test_y)))
+	plt.show()
+
+def get_amp_data_training_set(include_hemolysis=False):
+	amp_data = AMPDataset().data
 	amp_data = amp_data[[
 	    'bacterium', 'modifications', 'is_modified', 'value', 'sequence', 'datasource_has_modifications',
 	    'has_unusual_modification'
 	]]
 	amp_data = amp_data[amp_data.has_unusual_modification == False]
 	amp_data = amp_data[amp_data['datasource_has_modifications'] == True]
+
+	if include_hemolysis:
+		hemolysis = HemolysisDataset()
+		hemo_data = hemolysis.data
+		hemo_data['bacterium'] = ['H. sapiens' for i in range(len(hemo_data))]
+		hemo_data['value'] = hemo_data.log10_HC50
+		hemo_data['sequence'] = [hemo_data.Sequence[i].upper() for i in range(len(hemo_data))]
+		amp_data = pd.concat([amp_data,hemo_data],sort=False)
+		keep_list = []
+		for s in amp_data.sequence:
+		    keep = True
+		    for char in s:
+		        if char not in CHARACTER_DICT:
+		            keep = False
+		    keep_list.append(keep)
+		amp_data = amp_data[keep_list]
+
 	amp_data = amp_data.groupby(['bacterium', 'sequence']).mean().reset_index()
 
 	amp_data = amp_data[amp_data.bacterium.isin(amp_data.bacterium.value_counts().head(50).index)]
 	amp_data.loc[:, 'bacterium_id'] = amp_data.bacterium.astype('category').cat.codes
 	amp_data.loc[:,'bacterium_cat'] = amp_data.bacterium.astype('category')
+	return amp_data.sample(frac=1).reset_index(drop=True)
+
+
+def generate_pca_plot_from_data(embed_dim=10,include_human=False):
+	# Generates species embeddings based on the entire dataset (rather than a training test split, as this isn't really a training/test sort of thing)
+	amp_data = get_amp_data_training_set(include_hemolysis = include_human)
 	cat_dict = dict(enumerate(amp_data['bacterium_cat'].cat.categories))
 	early_stopping = EarlyStopping(monitor = 'val_loss', patience = 15)
 	bacterium_to_cat_id = {}
@@ -124,10 +187,17 @@ def generate_pca_plot_from_data(amp_data,embed_dim=10):
 	species_tags = ['Ecoli','Pa','Kp','St','Ab','Sm','Sa','Ml','Bs','Efaecium','Ca','Sc','Efaecalis','Ecloacae']
 	colors = ['b','b','b','b','b','g','g','g','g','g','k','k','g','b']
 	to_embed = [ec,pa,kp,st,ab,sm,sa,ml,bs,efaecium,ca,sc,efaecalis,ecloacae]
+	if include_human:
+		human = embedding_layer.get_weights()[0][bacterium_to_cat_id['H. sapiens']]
+		species_tags.append('Human')
+		colors.append('r')
+		to_embed.append(human)
+
 	to_embed = preprocessing.scale(to_embed)
 	make_PCA_plot(to_embed,species_tags,colors)
 
-generate_pca_plot_from_data(AMPDataset().data)
+# generate_pca_plot_from_data(include_human=True)
 
+compare_multispecies_to_single_species_training('P. aeruginosa')
 
 
